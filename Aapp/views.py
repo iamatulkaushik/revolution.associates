9b0dbefeffbc90bb56fdb42fad6fcd30cedb642e
Atul Kaushik <117714634+iamatulkaushik@users.noreply.github.com>
2026-07-25 19:48:03 +0530

    basic bug update

--- Aapp/views.py
+++ Aapp/views.py
@@ -49,7 +49,7 @@ def associate_login(request):
             auth_login(request, user)
             logger.info("Associate login: user='%s'", user.username)
             messages.success(request, 'Logged in successfully.')
-            return redirect('associate_dashboard')
+            return redirect('aapp_dashboard')
     else:
         form = loginForm()
     return render(request, 'associate_login.html', {'form': form})
