e9ab0249a4aa18fe28d81c097fa1c93762bf36cb
Atul Kaushik <117714634+iamatulkaushik@users.noreply.github.com>
2026-07-25 19:37:53 +0530

    aapp view update

--- Aapp/views.py
+++ Aapp/views.py
@@ -49,7 +49,7 @@ def associate_login(request):
             auth_login(request, user)
             logger.info("Associate login: user='%s'", user.username)
             messages.success(request, 'Logged in successfully.')
-            return redirect('aapp_dashboard')
+            return redirect('associate_dashboard')
     else:
         form = loginForm()
     return render(request, 'associate_login.html', {'form': form})
@@ -68,7 +68,7 @@ def logout(request):
 # ---------------------------------------------------------------------------
 
 @login_required
-def dashboard(request):
+def associate_dashboard(request):
     selected_company_id = request.session.get('selected_company_id')
     branches_count = 0
     departments_count = 0
