ba4f2108292a04d47f72fba6bacee28c19da03ca
Atul Kaushik <117714634+iamatulkaushik@users.noreply.github.com>
2026-08-15 22:40:53 +0530

    Remove 'Capp' from the migrate command in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py migrate Sapp Aapp Capp --fake
+python manage.py migrate Sapp Aapp --fake
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
