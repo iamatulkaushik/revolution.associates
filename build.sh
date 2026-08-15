c056a075cf3fbd0df738f51d0caa7fd3e4bda9c1
Atul Kaushik <117714634+iamatulkaushik@users.noreply.github.com>
2026-08-15 22:37:19 +0530

    Add fake migration for apps in build script
    Added fake migration for specified apps before collecting static files and running migrations.

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+python manage.py migrate Aapp Capp Cxapp Sapp --fake
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
