bba661e442ef9e46aa83c85ba1f4ac4218319485
Atul Kaushik <117714634+iamatulkaushik@users.noreply.github.com>
2026-08-15 22:51:48 +0530

    Update build.sh to remove fake migration
    Removed fake migration command from build script.

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py migrate Sapp Aapp --fake
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
