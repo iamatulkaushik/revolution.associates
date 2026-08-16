85bfb1e5016728f2fa8da40370c820a122f804bd
Atul Kaushik <117714634+iamatulkaushik@users.noreply.github.com>
2026-08-16 09:26:42 +0530

    Comment out app migrations in build.sh
    Comment out specific migration commands for apps.

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py migrate Sapp zero
-python manage.py migrate Aapp zero
-python manage.py migrate Capp zero
-python manage.py migrate Cxapp zero
+#python manage.py migrate Sapp zero
+#python manage.py migrate Aapp zero
+#python manage.py migrate Capp zero
+#python manage.py migrate Cxapp zero
 python manage.py collectstatic --no-input
-python manage.py makemigrations --no-input
+#python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py create_default_superuser
